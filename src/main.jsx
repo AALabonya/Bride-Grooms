89f69336bf652209276fa476aab1f0cff10d83e0
Labonya <1aklimakter@gmail.com>
2023-11-24 10:10:51 +0600

    I have completed login page

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,16 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import Router from './router/Router.jsx'
 import AuthProvider from './Provider/AuthProvider.jsx'
+import { HelmetProvider } from 'react-helmet-async'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
-    <RouterProvider router={Router}>
+      <HelmetProvider>
+      <RouterProvider router={Router}>
     </RouterProvider>
+      </HelmetProvider>
+    
     </AuthProvider>
     
 
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,10 +1,81 @@
+import { Link, useLocation, useNavigate } from "react-router-dom"
+import useAuth from "../../hooks/useAuth"
+import SocialLogin from "./SocialLogin"
+import { Helmet } from "react-helmet-async"
+import Swal from "sweetalert2"
 
 const Login = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+    const { signIn } = useAuth()
+    const navigate = useNavigate();
+    const location = useLocation();
 
+    const from = location.state?.from?.pathname || "/";
+    console.log('state in the location login page', location.state)
+
+
+    const handleLogin = event => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+        signIn(email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                Swal.fire({
+                    title: 'User Login Successful.',
+                    showClass: {
+                        popup: 'animate__animated animate__fadeInDown'
+                    },
+                    hideClass: {
+                        popup: 'animate__animated animate__fadeOutUp'
+                    }
+                });
+                navigate(from, { replace: true });
+            })
+    }
+
+    
+    return (
+        <>
+            <Helmet>
+                <title>Matrimony | Login</title>
+            </Helmet>
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col md:flex-row-reverse">
+                    <div className="text-center md:w-1/2 lg:text-left">
+                        <h1 className="text-5xl font-bold">Login now!</h1>
+                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    </div>
+                    <div className="card md:w-1/2 max-w-sm shadow-2xl bg-base-100">
+                        <form onSubmit={handleLogin} className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" name="email" placeholder="email" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" name="password" placeholder="password" className="input input-bordered" />
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                {/* TODO: apply disabled for re captcha */}
+                                <input disabled={false} className="btn btn-primary" type="submit" value="Login" />
+                            </div>
+                        </form>
+                        <p className="text-center font-bold text-lg"><small>New Here? Create an account <Link to="/signUp" className="text-blue-600">SignUp </Link></small></p>
+                       <SocialLogin/>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+  }
 export default Login;
